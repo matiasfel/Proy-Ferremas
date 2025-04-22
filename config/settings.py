da6b7be0191f07161a689f01f20661c4ef2b4a17
Matías Baxman <matiasbaxman@gmail.com>
2025-04-21 20:32:33 -0400

    fix: arreglos en settings.py

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Ferremas.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Ferremas.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
