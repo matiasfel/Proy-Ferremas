78205f3f044882d56a438edf5134a7f29b734b66
Matías Baxman <matiasbaxman@gmail.com>
2025-04-28 04:31:53 -0400

    add: png's y templates

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
